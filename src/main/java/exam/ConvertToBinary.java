5ed960f8c842dbd0a6d8448b26a17938ef4d37c0
Adhaness <86237693+Adhaness@users.noreply.github.com>
2021-06-22 13:28:43 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -3,7 +3,17 @@ package exam;
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+    // array to store binary number
+    int[] binaryNum = new int[32];
+ 
+    // counter for binary array
+    int i = 0;
+    while (n > 0) {
+        // storing remainder in binary array
+        binaryNum[i] = n % 2;
+        n = n / 2;
+        i++;
+    return "binaryNum";
     return "";
   }
 }
